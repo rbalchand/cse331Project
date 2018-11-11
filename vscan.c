1f4b0a76838516f3f52b031d22194fd3680e74c3
xiangli <xiangli1997@gmail.com>
2018-11-11 12:22:08 -0500

    scan direcotry for file

--- vscan.c
+++ vscan.c
@@ -1,28 +1,41 @@
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-int main (int argc, char* argv[]) {
-	FILE *fp;
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
+/* "readdir" etc. are defined here. */
+#include <dirent.h>
 
-	/* Open file for both reading and writing */
-	fp = fopen(argv[1], "r");
-    char *buffer = (char*)malloc(sizeof(char)*512);
-    /* Seek to the beginning of the file */
-    fseek(fp, 0, SEEK_SET);
+void scanDir(char *basePath)
+{
+    char path[1000];
+    struct dirent *dp;
+    DIR *dir = opendir(basePath);
 
-    /* Read and display data */
-	int x=0;
-	while(fread(&buffer, 1, 128, fp+x*128)==128)
-	{
-    	printf("%X\n",(unsigned int) buffer);
-   		x+=1;
-		printf("%d\n",x);
-		if(x>4){
-			break;
-		}
-		rewind(fp);
-	}
-    fclose(fp);
-   
-    return(0);
+    // Unable to open directory stream
+    if (!dir)
+        return;
+
+    while ((dp = readdir(dir)) != NULL)
+    {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
+        {
+            printf("%s\n", dp->d_name);
+
+            // Construct new path from our base path
+            strcpy(path, basePath);
+            strcat(path, "/");
+            strcat(path, dp->d_name);
+
+            scanDir(path);
+        }
+    }
+
+    closedir(dir);
+}
+
+ int main (int argc, char* argv[])
+{
+    scanDir(argv[1]);
+    return 0;
 }
